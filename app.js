7f254bc9952e1d4d6568db4457059550e519995a
Kyle Measner <kyle.measner@gmail.com>
2017-12-28 19:58:42 -0800

    Replaced httpPromise library with request-process Node module.

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const express = require('express');
 
 const TeamCowboy = require('./lib/teamCowboy/teamCowboy').TeamCowboy;
 
+const PUBLIC_API_KEY = process.env.PUBLIC_API_KEY;
+const PRIVATE_API_KEY = process.env.PRIVATE_API_KEY;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+
 const app = express();
 
 app.get('/', function(req, res) {
-    const teamCowboy = new TeamCowboy(process.env.PUBLIC_API_KEY, process.env.PRIVATE_API_KEY);
+    const teamCowboy = new TeamCowboy(PUBLIC_API_KEY, PRIVATE_API_KEY);
 
-    teamCowboy.login(process.env.USERNAME, process.env.PASSWORD).then(function(userToken) {
+    teamCowboy.login(USERNAME, PASSWORD).then(function(userToken) {
         teamCowboy.getNextGame().then(function(game) {
             res.json(game);
         });
